f9c22a8d384e52599495d67f5fb55f953cf042cd
nairx <nairxprv@gmail.com>
2025-05-25 21:18:47 +0530

    Updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,8 @@ export default function Header() {
       <Link to="/app9">App9</Link>-
       <Link to="/app10">App10</Link>-
       <Link to="/app11">App11</Link>-
-      <Link to="/app12">App12</Link>
+      <Link to="/app12">App12</Link>-
+       <Link to="/app13">App13</Link>
     </div>
   );
 }
